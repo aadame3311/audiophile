7299d63177983c795b96ccf88cc34917b059f68d
Antonio Adame P <a.adame59@yahoo.com>
2020-04-07 19:39:06 -0500

    added env to port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
 const youtubedl = require('youtube-dl');
